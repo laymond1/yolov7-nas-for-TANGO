079af0499e06470e963665a79f4e4f9b2c764cba
Hanyong Lee <glhy0718@gmail.com>
2023-05-17 13:38:06 +0900

    update

--- latency_predictor/arch_utils.py
+++ latency_predictor/arch_utils.py
@@ -1,21 +1,27 @@
 from nas.supernet.yolo import *
 
+
 class skin_backbone(nn.Module):
     def __init__(self, arch, save):
         super(skin_backbone, self).__init__()
         self.model = arch
         self.save = save
-    
+
     def forward(self, x):
-        y=[]
+        y = []
         for m in self.model:
             if m.f != -1:  # if not from previous layer
-                x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
+                x = (
+                    y[m.f]
+                    if isinstance(m.f, int)
+                    else [x if j == -1 else y[j] for j in m.f]
+                )  # from earlier layers
             x = m(x)  # run
             y.append(x if m.i in self.save else None)
         self.y = y
         return x
-    
+
+
 class skin_head(nn.Module):
     def __init__(self, arch, save):
         super(skin_head, self).__init__()
@@ -24,34 +30,48 @@ class skin_head(nn.Module):
         self.y = [None for _ in range(27)]
         self.y[12] = torch.rand(1, 512, 80, 80)
         self.y[19] = torch.rand(1, 1024, 40, 40)
-    
+
     def forward(self, x):
         device = x.get_device()
         if device >= 0:
             device = "cuda:" + str(device)
         else:
             device = "cpu"
-        y=self.y
+        y = self.y
         for m in self.model:
             if m.f != -1:  # if not from previous layer
-                x = y[m.f].to(device) if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
+                x = (
+                    y[m.f].to(device)
+                    if isinstance(m.f, int)
+                    else [x if j == -1 else y[j] for j in m.f]
+                )  # from earlier layers
             x = m(x)  # run
             y.append(x if m.i in self.save else None)
         return x
+
 
 def split_backbone_head(subnet):
     # This is only used to make the latency LUT.
     # Therefore, the trained weights are not used.
     d = subnet.yaml
-    ch = [d['ch']]
-    anchors, nc, gd, gw = d['anchors'], d['nc'], d['depth_multiple'], d['width_multiple']
+    ch = [d["ch"]]
+    anchors, nc, gd, gw = (
+        d["anchors"],
+        d["nc"],
+        d["depth_multiple"],
+        d["width_multiple"],
+    )
 
-    na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors
+    na = (
+        (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors
+    )  # number of anchors
     no = na * (nc + 5)  # number of outputs = anchors * (classes + 5)
 
-    delimiter = len(d['backbone'])
+    delimiter = len(d["backbone"])
     blayers, hlayers, save, c2 = [], [], [], ch[-1]  # layers, savelist, ch out
-    for i, (f, n, m, args) in enumerate(d['backbone'] +d['head']):  # from, number, module, args
+    for i, (f, n, m, args) in enumerate(
+        d["backbone"] + d["head"]
+    ):  # from, number, module, args
         # if i == delimiter:
         #     save.extend("delimiter")
         m = eval(m) if isinstance(m, str) else m  # eval strings
@@ -62,39 +82,107 @@ def split_backbone_head(subnet):
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [nn.Conv2d, Conv, DyConv, RobustConv, RobustConv2, DWConv, GhostConv, RepConv, RepConv_OREPA, DownC, 
-                 SPP, SPPF, SPPCSPC, GhostSPPCSPC, MixConv2d, Focus, Stem, GhostStem, CrossConv, 
-                 Bottleneck, BottleneckCSPA, BottleneckCSPB, BottleneckCSPC, 
-                 RepBottleneck, RepBottleneckCSPA, RepBottleneckCSPB, RepBottleneckCSPC,  
-                 Res, ResCSPA, ResCSPB, ResCSPC, 
-                 RepRes, RepResCSPA, RepResCSPB, RepResCSPC, 
-                 ResX, ResXCSPA, ResXCSPB, ResXCSPC, 
-                 RepResX, RepResXCSPA, RepResXCSPB, RepResXCSPC, 
-                 Ghost, GhostCSPA, GhostCSPB, GhostCSPC,
-                 SwinTransformerBlock, STCSPA, STCSPB, STCSPC,
-                 SwinTransformer2Block, ST2CSPA, ST2CSPB, ST2CSPC]:
+        if m in [
+            nn.Conv2d,
+            Conv,
+            DyConv,
+            RobustConv,
+            RobustConv2,
+            DWConv,
+            GhostConv,
+            RepConv,
+            RepConv_OREPA,
+            DownC,
+            SPP,
+            SPPF,
+            SPPCSPC,
+            GhostSPPCSPC,
+            MixConv2d,
+            Focus,
+            Stem,
+            GhostStem,
+            CrossConv,
+            Bottleneck,
+            BottleneckCSPA,
+            BottleneckCSPB,
+            BottleneckCSPC,
+            RepBottleneck,
+            RepBottleneckCSPA,
+            RepBottleneckCSPB,
+            RepBottleneckCSPC,
+            Res,
+            ResCSPA,
+            ResCSPB,
+            ResCSPC,
+            RepRes,
+            RepResCSPA,
+            RepResCSPB,
+            RepResCSPC,
+            ResX,
+            ResXCSPA,
+            ResXCSPB,
+            ResXCSPC,
+            RepResX,
+            RepResXCSPA,
+            RepResXCSPB,
+            RepResXCSPC,
+            Ghost,
+            GhostCSPA,
+            GhostCSPB,
+            GhostCSPC,
+            SwinTransformerBlock,
+            STCSPA,
+            STCSPB,
+            STCSPC,
+            SwinTransformer2Block,
+            ST2CSPA,
+            ST2CSPB,
+            ST2CSPC,
+        ]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
 
             args = [c1, c2, *args[1:]]
-            if m in [DownC, SPPCSPC, GhostSPPCSPC, 
-                     BottleneckCSPA, BottleneckCSPB, BottleneckCSPC, 
-                     RepBottleneckCSPA, RepBottleneckCSPB, RepBottleneckCSPC, 
-                     ResCSPA, ResCSPB, ResCSPC, 
-                     RepResCSPA, RepResCSPB, RepResCSPC, 
-                     ResXCSPA, ResXCSPB, ResXCSPC, 
-                     RepResXCSPA, RepResXCSPB, RepResXCSPC,
-                     GhostCSPA, GhostCSPB, GhostCSPC,
-                     STCSPA, STCSPB, STCSPC,
-                     ST2CSPA, ST2CSPB, ST2CSPC]:
+            if m in [
+                DownC,
+                SPPCSPC,
+                GhostSPPCSPC,
+                BottleneckCSPA,
+                BottleneckCSPB,
+                BottleneckCSPC,
+                RepBottleneckCSPA,
+                RepBottleneckCSPB,
+                RepBottleneckCSPC,
+                ResCSPA,
+                ResCSPB,
+                ResCSPC,
+                RepResCSPA,
+                RepResCSPB,
+                RepResCSPC,
+                ResXCSPA,
+                ResXCSPB,
+                ResXCSPC,
+                RepResXCSPA,
+                RepResXCSPB,
+                RepResXCSPC,
+                GhostCSPA,
+                GhostCSPB,
+                GhostCSPC,
+                STCSPA,
+                STCSPB,
+                STCSPC,
+                ST2CSPA,
+                ST2CSPB,
+                ST2CSPC,
+            ]:
                 args.insert(2, n)  # number of repeats
                 n = 1
         elif m is BBoneELAN:
-            c1, c2 = ch[f], int(args[0]*(args[-1]+1))
+            c1, c2 = ch[f], int(args[0] * (args[-1] + 1))
             args = [c1, *args]
         elif m is HeadELAN:
-            c1, c2 = ch[f], int((args[0]*2) + (args[0]/2 * (args[-1]-1)))
+            c1, c2 = ch[f], int((args[0] * 2) + (args[0] / 2 * (args[-1] - 1)))
             args = [c1, *args]
         elif m is nn.BatchNorm2d:
             args = [ch[f]]
@@ -119,11 +207,20 @@ def split_backbone_head(subnet):
         else:
             c2 = ch[f]
 
-        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  # module
-        t = str(m)[8:-2].replace('__main__.', '')  # module type
+        m_ = (
+            nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)
+        )  # module
+        t = str(m)[8:-2].replace("__main__.", "")  # module type
         np = sum([x.numel() for x in m_.parameters()])  # number params
-        m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
-        save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+        m_.i, m_.f, m_.type, m_.np = (
+            i,
+            f,
+            t,
+            np,
+        )  # attach index, 'from' index, type, number params
+        save.extend(
+            x % i for x in ([f] if isinstance(f, int) else f) if x != -1
+        )  # append to savelist
         if i < delimiter:
             blayers.append(m_)
         else:
